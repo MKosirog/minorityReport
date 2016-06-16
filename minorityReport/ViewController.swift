28f4b7a939cbe9bd4b77293e514f6855f41fd320
Mark C. Kosirog <mark.kosirog@vg-mbp-kosirog.local>
2016-06-16 12:08:26 -0500

    Added Alert

--- minorityReport/ViewController.swift
+++ minorityReport/ViewController.swift
@@ -10,16 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var theFutureLabel: UILabel!
+    @IBOutlet weak var preCogsLabel: UILabel!
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func onDrag(sender: UIPanGestureRecognizer)
+    {
+        let point = sender.locationInView(view)
+        theFutureLabel.center = point
+        if sender.state == .Ended
+        {
+           UIView.animateWithDuration(0.5, animations:
+            {
+                self.theFutureLabel.center = self.view.center
+            })
+        }
+        else if CGRectContainsPoint(preCogsLabel.frame, point)
+        {
+            
+            let myAlert = UIAlertController(title: "Minority Report", message: "PreCog Detected!", preferredStyle: UIAlertControllerStyle.Alert)
+            myAlert.addAction(UIAlertAction(title: "Arrest Them!", style: .Default, handler: nil))
+            myAlert.addAction(UIAlertAction(title: "Let Em Go!", style: .Default, handler: nil))
+            presentViewController(myAlert, animated: true, completion: nil)
+        }
     }
-
-
-}
+    
+    
+   }
 
